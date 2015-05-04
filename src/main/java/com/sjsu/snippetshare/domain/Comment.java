49f91f703007c194d9c8ba105920dc1d786d9473
sindhu_nk <sindhunkashyap@gmail.com>
2015-05-04 13:07:54 -0700

    WIP

---
+++ src/main/java/com/sjsu/snippetshare/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.sjsu.snippetshare.domain;
+
+import org.bson.types.ObjectId;
+
+public class Comment 
+{
+
+	String text;
+	String commentID;
+	String ownerID;
+	
+	public String getOwnerID() {
+		return ownerID;
+	}
+	public void setOwnerID(String ownerID) {
+		this.ownerID = ownerID;
+	}
+	public String getText() 
+	{
+		return text;
+	}
+	public void setText(String text) 
+	{
+		this.text = text;
+	}
+	public String getCommentID() 
+	{
+		return commentID;
+	}
+	public void setCommentID(String commentID) 
+	{
+		this.commentID = commentID;
+	}
+}
