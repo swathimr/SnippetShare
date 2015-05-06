7be04f150cb1ab8e20f0cf498c9aab51267c019c
sindhu_nk <sindhunkashyap@gmail.com>
2015-05-05 18:08:44 -0700

    updated comments

--- src/main/java/com/sjsu/snippetshare/controller/CommentsController.java
+++ src/main/java/com/sjsu/snippetshare/controller/CommentsController.java
@@ -15,30 +15,30 @@ import com.sjsu.snippetshare.service.CommentsHandler;
 public class CommentsController {
 
 	CommentsHandler handler;
-	@RequestMapping(value="/comments/{user}/{board}/{snippet}",method=RequestMethod.PUT)
-	public Comment createComment(@PathVariable("user") String user,@PathVariable("board") String board,
+	@RequestMapping(value="/createComment/{user}/{board}/{snippet}",method=RequestMethod.POST)
+	public Comment createComment(@PathVariable("user") String commentOwner,@PathVariable("board") String board,
 			@PathVariable("snippet") String snippet,@ModelAttribute Comment comment,Model model) 
 					throws UnknownHostException
 	{
-		//System.out.println("yup in here"+user.getEmail());
+		System.out.println("yup in here"+commentOwner);
 		handler= new CommentsHandler();
 		if(!comment.getText().equals(null))
 		{
-			comment = handler.createComment(comment,user,board,snippet);
+			comment = handler.createComment(comment,commentOwner,board,snippet);
 		}
 		
 		return comment;
 	}
 	
-	@RequestMapping(value="/comments/{id1}/{id2}",method=RequestMethod.PUT)
-	public Comment updateComment(@PathVariable("id1") String snippetOwner,@PathVariable("id2") String comment_id,
-			@ModelAttribute Comment comment,Model model) throws UnknownHostException
+	@RequestMapping(value="/updateComment/{board}/{snippet}/{comment}",method=RequestMethod.PUT)
+	public Comment updateComment(@PathVariable("snippet") String snippetId,
+			@PathVariable("comment") String commentId,@ModelAttribute Comment comment,Model model) throws UnknownHostException
 	{
 		//System.out.println("yup in here"+user.getEmail());
 		handler= new CommentsHandler();
 		if(!comment.getText().equals(null))
 		{
-			comment = handler.updateComment(comment,snippetOwner,comment_id);
+			comment = handler.updateComment(comment,snippetId,commentId);
 		}
 		
 		return comment;
--- src/main/java/com/sjsu/snippetshare/service/CommentsHandler.java
+++ src/main/java/com/sjsu/snippetshare/service/CommentsHandler.java
@@ -15,16 +15,17 @@ public class CommentsHandler {
 	
 	DBCollection coll; 
 	BasicDBObject doc;
-	public Comment createComment(Comment comment, String user, String board,String snippet) throws UnknownHostException 
+	public Comment createComment(Comment comment, String commentOwner, String board,String snippet) throws UnknownHostException 
 	{
 		coll = MongoFactory.getConnection().getCollection("Board");
 		boolean updated = false;
 		String comment_id = new ObjectId().toString();
 		comment.setCommentId(comment_id);
+		comment.setOwnerId(commentOwner);
 		BasicDBObject newComment = new BasicDBObject("commentId",comment.getCommentId()).
 				append("text",comment.getText()).append("ownerId",comment.getOwnerId());
-		DBObject updateQuery = new BasicDBObject("_id", user)
-							.append("Snippets.snippetId", new ObjectId(snippet));
+		DBObject updateQuery = new BasicDBObject("_id", new ObjectId(board))
+							.append("snippets.snippetId", snippet);
 		BasicDBObject updateCommand =
 				new BasicDBObject("$push", new BasicDBObject("Snippets.$.comments", newComment));
 		WriteResult collDB = coll.update(updateQuery, updateCommand);
@@ -40,20 +41,15 @@ public class CommentsHandler {
 		return comment;
 	}
 	
-	public String convertToString(ObjectId id)
-	{
-		return id.toString();
-	}
-	
-	public Comment updateComment(Comment comment, String snippetOwner,String comment_id) throws UnknownHostException 
+	public Comment updateComment(Comment comment, String snippetId,String commentId) throws UnknownHostException 
 	{
 		
 		coll = MongoFactory.getConnection().getCollection("Board");
 		boolean updated = false;
 		//BasicDBObject newComment = new BasicDBObject("text",comment.getText()).append("owner_id",comment.getOwnerID());
 		BasicDBObject updateComment = new BasicDBObject("text",comment.getText());
-		DBObject updateQuery = new BasicDBObject("Snippets.owner_id", new ObjectId(snippetOwner)).
-				append("Snippets.comments.comment_id", new ObjectId(comment_id));
+		DBObject updateQuery = new BasicDBObject("snippets.snippetId", snippetId).
+				append("snippets.comments.commentId", commentId);
 		BasicDBObject updateCommand =
 				new BasicDBObject("$push", new BasicDBObject("Snippets.comments.$.text", updateComment));
 		WriteResult collDB = coll.update(updateQuery, updateCommand);
